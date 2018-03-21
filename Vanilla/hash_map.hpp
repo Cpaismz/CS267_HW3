e1ae4b5ac895ea364209f1b1948058077bda7cd4
Alexander Wu <awu0012@gmail.com>
2018-03-20 17:50:49 -0700

    minimize memory footprint

--- Vanilla/hash_map.hpp
+++ Vanilla/hash_map.hpp
@@ -7,16 +7,16 @@
 
 struct HashMap {
     std::vector<upcxx::global_ptr<kmer_pair>> data;
-    std::vector<int> used;
+    std::vector<upcxx::global_ptr<std::int32_t>> used;
     std::vector<kmer_pair> to_insert;
+    // for hashtable miss tracking
     int num_uses;
     int tot_hits;
   size_t my_size;
-  std::mutex lock;
 
   size_t size() const noexcept;
 
-  HashMap(size_t size, std::vector<upcxx::global_ptr<kmer_pair>> _data);
+  HashMap(size_t size, std::vector<upcxx::global_ptr<kmer_pair>> _data, std::vector<upcxx::global_ptr<std::int32_t>> _used);
 
   // Most important functions: insert and retrieve
   // k-mers from the hash table.
@@ -27,34 +27,23 @@ struct HashMap {
   bool find(const pkmer_t &key_kmer, kmer_pair &val_kmer);
 
   // Helper functions
-
+    upcxx::global_ptr<kmer_pair> getSlotAddr(uint64_t slot);
+    upcxx::global_ptr<std::int32_t> getUsedSlotAddr(uint64_t slot);
   // Write and read to a logical data slot in the table.
-  void write_slot(uint64_t slot, const kmer_pair &kmer, int node_num);
-  kmer_pair read_slot(uint64_t slot, int node_num);
+  void write_slot(uint64_t slot, const kmer_pair &kmer);
+  kmer_pair read_slot(uint64_t slot);
 
   // Request a slot or check if it's already used.
-  bool request_slot(uint64_t slot, int node_num);
-  bool slot_used(uint64_t slot, int node_num);
+  bool request_slot(uint64_t slot);
+  bool slot_used(uint64_t slot);
 };
 
-HashMap::HashMap(size_t size, std::vector<upcxx::global_ptr<kmer_pair>> _data) {
+HashMap::HashMap(size_t size, std::vector<upcxx::global_ptr<kmer_pair>> _data, std::vector<upcxx::global_ptr<std::int32_t>> _used) {
   my_size = size;
   data = _data;
-  used.resize(size, 0);
+  used = _used;
   num_uses = 0;
   tot_hits = 0;
-}
-
-void HashMap::queue(const kmer_pair &kmer) {
-    lock.lock();
-    to_insert.push_back(kmer);
-    lock.unlock();
-}
-
-void HashMap::reduce() {
-    for (auto & a : to_insert) {
-        insert(a);
-    }
 }
 
 // only insert "locally"
@@ -62,27 +51,37 @@ void HashMap::insert(const kmer_pair &kmer) {
   uint64_t hash = kmer.hash();
   uint64_t probe = 0;
   bool success = false;
-  int node_num = (hash % size()) / (size() / upcxx::rank_n());
 
   do {
     uint64_t slot = (hash + probe++) % size();
-    success = request_slot(slot, node_num);
+    success = request_slot(slot);
     if (success) {
-      write_slot(slot, kmer, node_num);
+      write_slot(slot, kmer);
     }
   } while (!success && probe < size());
+}
+
+upcxx::global_ptr<kmer_pair> HashMap::getSlotAddr(uint64_t slot) {
+    int node_num = slot / (size() / upcxx::rank_n());
+    int offset = slot % (size() / upcxx::rank_n());
+    return data[node_num] + offset;
+}
+
+upcxx::global_ptr<std::int32_t> HashMap::getUsedSlotAddr(uint64_t slot) {
+    int node_num = slot / (size() / upcxx::rank_n());
+    int offset = slot % (size() / upcxx::rank_n());
+    return used[node_num] + offset;
 }
 
 bool HashMap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
     num_uses++;
   uint64_t hash = key_kmer.hash();
   uint64_t probe = 0;
-  int node_num = (hash % size()) / (size() / upcxx::rank_n());
   bool success = false;
   do {
     uint64_t slot = (hash + probe++) % size();
     //if (slot_used(slot, node_num)) {
-      val_kmer = read_slot(slot, node_num);
+      val_kmer = read_slot(slot);
       if (val_kmer.kmer == key_kmer) {
         success = true;
       }
@@ -92,8 +91,10 @@ bool HashMap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
   return success;
 }
 
-bool HashMap::slot_used(uint64_t slot, int node_num) {
-    return used[slot] != 0;
+bool HashMap::slot_used(uint64_t slot) {
+    // this function is currently not used
+    return false;
+    //return used[slot] != 0;
     /*
     upcxx::future<int> res = upcxx::rget(used[node_num] + slot);
     res.wait();
@@ -101,32 +102,24 @@ bool HashMap::slot_used(uint64_t slot, int node_num) {
   // return used[slot] != 0;
 }
 
-void HashMap::write_slot(uint64_t slot, const kmer_pair &kmer, int node_num) {
-    upcxx::rput(kmer, data[node_num] + slot).wait();
+void HashMap::write_slot(uint64_t slot, const kmer_pair &kmer) {
+    upcxx::rput(kmer, getSlotAddr(slot)).wait();
 }
 
 
-kmer_pair HashMap::read_slot(uint64_t slot, int node_num) {
-    upcxx::future<kmer_pair> res = upcxx::rget(data[node_num] + slot);
+kmer_pair HashMap::read_slot(uint64_t slot) {
+    upcxx::future<kmer_pair> res = upcxx::rget(getSlotAddr(slot));
     res.wait();
     return res.result();
   //return data[slot];
 }
 
-bool HashMap::request_slot(uint64_t slot, int node_num) {
-    /*
-    upcxx::future<int> res =  upcxx::rget(used[node_num] + slot);
+bool HashMap::request_slot(uint64_t slot) {
+    upcxx::future<std::int32_t> res =  upcxx::atomic_fetch_add(getUsedSlotAddr(slot), 1, std::memory_order_relaxed);
     res.wait();
   if (res.result() != 0) {
     return false;
   } else {
-    upcxx::rput(1, used[node_num] + slot).wait();
-    return true;
-  }*/
-  if (used[slot] != 0) {
-    return false;
-  } else {
-    used[slot] = 1;
     return true;
   }
 }
